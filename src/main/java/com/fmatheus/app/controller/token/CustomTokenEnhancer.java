4c182bbeecc59921cae285e63bb970bbe043bd3f
Fernando Matheus <fernando.matheuss@hotmail.com>
2022-02-06 01:06:28 -0300

    Inclusão de novo campo no token.

--- src/main/java/com/fmatheus/app/controller/token/CustomTokenEnhancer.java
+++ src/main/java/com/fmatheus/app/controller/token/CustomTokenEnhancer.java
@@ -2,9 +2,7 @@ package com.fmatheus.app.controller.token;
 
 import com.fmatheus.app.controller.constant.ValuePropertiesConstant;
 import com.fmatheus.app.controller.security.UserSecurity;
-import com.fmatheus.app.controller.storage.FileServiceStorage;
 import com.fmatheus.app.controller.util.AppUtil;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
 import org.springframework.security.oauth2.common.OAuth2AccessToken;
@@ -17,9 +15,6 @@ import java.util.stream.Collectors;
 
 public class CustomTokenEnhancer implements TokenEnhancer {
 
-    @Autowired
-    private FileServiceStorage fileServiceStorage;
-
     @Value(ValuePropertiesConstant.OPENAPI_VERSION)
     private String version;
 
@@ -28,13 +23,14 @@ public class CustomTokenEnhancer implements TokenEnhancer {
 
         var user = (UserSecurity) authentication.getPrincipal();
 
-        var roles = user.getUser().getRoleCollection().stream().map(map -> map.getRole().toUpperCase()).collect(Collectors.toList());
+        var roles = user.getUser().getRoleCollection().stream().map(map -> map.getRoleUser().toUpperCase()).collect(Collectors.toList());
 
         Map<String, Object> info = new HashMap<>();
         info.put("name", AppUtil.convertFirstUppercaseCharacter(user.getUser().getPerson().getName()));
         info.put("firstName", AppUtil.returnFirstWord(AppUtil.convertFirstUppercaseCharacter(user.getUser().getPerson().getName())));
         info.put("username", AppUtil.convertAllLowercaseCharacters(user.getUsername()));
         info.put("roles", roles);
+        info.put("version", version);
 
         ((DefaultOAuth2AccessToken) accessToken).setAdditionalInformation(info);
 
