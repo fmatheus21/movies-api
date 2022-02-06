47274dde6b036ce33de6bf17de61dfcdad209ccf
Fernando Matheus <fernando.matheuss@hotmail.com>
2022-02-06 01:09:34 -0300

    Alterações nos atributos do enum.

--- src/main/java/com/fmatheus/app/controller/enumerable/MessagesEnum.java
+++ src/main/java/com/fmatheus/app/controller/enumerable/MessagesEnum.java
@@ -1,45 +1,41 @@
 package com.fmatheus.app.controller.enumerable;
 
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 /**
- * Enum que representa as mensagens padrao da api que se encontrar em
- * messages.properties
+ * Enum que representa as mensagens padrao da api que se encontrar em i18n/messages.yml
  *
  * @author Fernando Matheus
  */
 public enum MessagesEnum {
 
-    SUCCESS_CREATE(201, "Created", "CREATED", "message.success.create"),
-    SUCCESS_UPDATE(201, "Created", "CREATED", "message.success.update"),
-    SUCCESS_DELETE(200, "OK", "OK", "message.success.delete"),
-    ERROR_INTERNAL(500, "Internal Server", "INTERNAL SERVER ERROR", "message.error.internal-server"),
-    ERROR_FILE_STORAGE(500, "Internal Server", "INTERNAL SERVER ERROR", "message.error.file-storage"),
-    ERROR_FORBIDDEN(403, "Forbidden", "FORBIDDEN", "message.error.forbidden"),
-    ERROR_BAD_REQUEST(400, "Bad Request", "BAD REQUEST", "message.error.bad-request"),
-    ERROR_NOT_PERMISSION(403, "Forbidden", "FORBIDDEN", "message.error.not-permission"),
-    ERROR_NOT_FOUND(400, "Bad Request", "BAD REQUEST", "message.error.not-found"),
-    ERROR_NOT_READABLE(400, "Bad Request", "BAD REQUEST", "message.error.not-readable"),
-    ERROR_USER_NOTFOUND(400, "Bad Request", "BAD REQUEST", "message.error.user-notfound"),
-    ERROR_USER_INACTIVE(400, "Bad Request", "BAD REQUEST", "message.error.user-inactive"),
-    ERROR_NOT_FOUND_FILE(400, "Bad Request", "BAD REQUEST", "message.error.not-found-file");
+    SUCCESS_CREATE(HttpStatus.CREATED, "message.success.create"),
+    SUCCESS_UPDATE(HttpStatus.CREATED, "message.success.update"),
+    SUCCESS_DELETE(HttpStatus.OK, "message.success.delete"),
+    ERROR_INTERNAL(HttpStatus.INTERNAL_SERVER_ERROR, "message.error.internal-server"),
+    ERROR_FILE_STORAGE(HttpStatus.INTERNAL_SERVER_ERROR, "message.error.file-storage"),
+    ERROR_FORBIDDEN(HttpStatus.FORBIDDEN, "message.error.forbidden"),
+    ERROR_BAD_REQUEST(HttpStatus.BAD_REQUEST, "message.error.bad-request"),
+    ERROR_CODE_IMDB_EXIST(HttpStatus.BAD_REQUEST, "message.error.code-imdb-exist"),
+    ERROR_NOT_PERMISSION(HttpStatus.FORBIDDEN, "message.error.not-permission"),
+    ERROR_NOT_FOUND(HttpStatus.BAD_REQUEST, "message.error.not-found"),
+    ERROR_NOT_READABLE(HttpStatus.BAD_REQUEST, "message.error.not-readable"),
+    ERROR_USER_NOTFOUND(HttpStatus.BAD_REQUEST, "message.error.user-notfound"),
+    ERROR_USER_INACTIVE(HttpStatus.BAD_REQUEST, "message.error.user-inactive"),
+    ERROR_NOT_FOUND_FILE(HttpStatus.BAD_REQUEST, "message.error.not-found-file"),
+    ERROR_NOT_UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "message.error.unauthorized"),
+    ERROR_COULD_NOT_READ(HttpStatus.INTERNAL_SERVER_ERROR, "message.error.could-not-read");
+
 
     @Getter
-    private final int httpCode;
-
-    @Getter
-    private final String httpDesctription;
-
-    @Getter
-    private final String httpCause;
+    private final HttpStatus httpSttus;
 
     @Getter
     private final String message;
 
-    MessagesEnum(int httpCode, String httpDesctription, String httpCause, String message) {
-        this.httpCode = httpCode;
-        this.httpDesctription = httpDesctription;
-        this.httpCause = httpCause;
+    MessagesEnum(HttpStatus httpSttus, String message) {
+        this.httpSttus = httpSttus;
         this.message = message;
     }
 
