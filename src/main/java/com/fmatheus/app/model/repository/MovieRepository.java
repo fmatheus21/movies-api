d2bdf4930d7fb1827ca0bc2d5a77e6550e1e1c70
Fernando Matheus <fernando.matheuss@hotmail.com>
2022-02-06 22:10:45 -0300

    Implementação de paginação.

--- src/main/java/com/fmatheus/app/model/repository/MovieRepository.java
+++ src/main/java/com/fmatheus/app/model/repository/MovieRepository.java
@@ -1,13 +1,14 @@
 package com.fmatheus.app.model.repository;
 
 import com.fmatheus.app.model.entity.Movie;
+import com.fmatheus.app.model.repository.query.MovieRepositoryQuery;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface MovieRepository extends JpaRepository<Movie, Integer> {
+public interface MovieRepository extends JpaRepository<Movie, Integer>, MovieRepositoryQuery {
 
     Optional<Movie> findByCodeImdb(String codeIndb);
 
