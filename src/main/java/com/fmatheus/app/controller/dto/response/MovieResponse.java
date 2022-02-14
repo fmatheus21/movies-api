de3d7dba0d52a8ce37170833213ee197014f0800
Fernando Matheus <fernando.matheuss@hotmail.com>
2022-02-14 01:51:29 -0300

    Alteração no método inclundo converter.

--- src/main/java/com/fmatheus/app/controller/dto/response/MovieResponse.java
+++ src/main/java/com/fmatheus/app/controller/dto/response/MovieResponse.java
@@ -1,37 +1,46 @@
 package com.fmatheus.app.controller.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.fmatheus.app.controller.converter.MovieConverter;
 import com.fmatheus.app.controller.dto.MovieDto;
 import com.fmatheus.app.controller.hateoas.model.MovieRepresentationModel;
-import com.fmatheus.app.controller.storage.FilesStorageService;
-import com.fmatheus.app.controller.util.MethodGlobalUtil;
 import com.fmatheus.app.model.entity.Movie;
 import lombok.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.hateoas.RepresentationModel;
+import org.springframework.stereotype.Component;
 
+
+@Component
+@EqualsAndHashCode(callSuper = false)
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@JsonIgnoreProperties({"converter"})
 @JsonPropertyOrder({"_links", "movie"})
 public class MovieResponse extends RepresentationModel<MovieResponse> {
 
     private MovieDto movie;
 
-    public static MovieResponse converterForResponse(Movie movie, MethodGlobalUtil methodGlobalUtil, FilesStorageService filesStorageService) {
-        return hateoas(MovieResponse.builder()
-                .movie(MovieDto.converterToDto(movie, methodGlobalUtil, filesStorageService))
+    @Autowired
+    private MovieConverter converter;
+
+    public MovieResponse converterForResponse(Movie movie) {
+        return this.hateoas(MovieResponse.builder()
+                .movie(this.converter.converterToDto(movie))
                 .build());
     }
 
-    public static Page<MovieResponse> converterListForResponse(Page<Movie> movies, MethodGlobalUtil methodGlobalUtil, FilesStorageService filesStorageService) {
-        return movies.map(map -> MovieResponse.converterForResponse(map, methodGlobalUtil, filesStorageService));
+    private MovieResponse hateoas(MovieResponse dto) {
+        return new MovieRepresentationModel().hateoas(dto, dto.getMovie().getId());
     }
 
-    private static MovieResponse hateoas(MovieResponse dto) {
-        return new MovieRepresentationModel().hateoas(dto, dto.getMovie().getId());
+    public Page<MovieResponse> converterListForResponse(Page<Movie> movies) {
+        return movies.map(this::converterForResponse);
     }
 
 
